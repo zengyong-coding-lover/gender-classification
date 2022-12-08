c4bb72978b4c6e2c5ac8e00aa0f5c3b8d53d1783
zengyong <2595650269@qq.com>
2022-12-08 17:39:59 +0800

    ok for test submit

--- dataloader.py
+++ dataloader.py
@@ -24,3 +24,17 @@ class ImageDataset(Dataset):
         if self.target_transform:
             label = self.target_transform(label)
         return image, label      
+
+class TestImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_name = self.img_labels['image_id'][idx]
+        img_path = os.path.join(self.img_dir, img_name)
+        image = read_image(img_path) / 255
+        return img_name, image  
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Author: zengyong 2595650269@qq.com
 Date: 2022-12-07 09:40:42
 LastEditors: zengyong 2595650269@qq.com
-LastEditTime: 2022-12-08 11:26:40
+LastEditTime: 2022-12-08 17:38:09
 FilePath: \gender-classification\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -16,22 +16,25 @@ import torch
 from torch import nn
 from matplotlib import pylab as plt
 import seaborn as sns
+models = ["ResNet", "XNet"]
+num_epochs = 2 
+batchsize = 50
+device = 'cpu'
+lr = 0.1
+train_dir = './bitmoji-faces-gender-recognition/train.csv'
+train_image_dir = './bitmoji-faces-gender-recognition/BitmojiDataset/trainimages/'
+test_dir = './bitmoji-faces-gender-recognition/sample_submission.csv'
+test_image_dir = './bitmoji-faces-gender-recognition/BitmojiDataset/testimages'
 
 if __name__ == '__main__':
-    models = ["ResNet", "XNet"]
-    init_args = [(), (3, 1)]
-    num_epochs = 3 
-    batchsize = 50
-    device = 'cpu'
-    lr = 0.1
     transform = lambda X: torch.tensor(X, device=device, dtype=torch.float32)
-    dataset = ImageDataset('./bitmoji-faces-gender-recognition/train.csv', 
-            r'.\bitmoji-faces-gender-recognition/BitmojiDataset/trainimages/', transform, transform)
-    train_len, test_len = int(len(dataset) * 0.8), int(len(dataset) * 0.2)
-    train, test = Data.random_split(dataset, [train_len, test_len])
-    #print(test.shape)
+    dataset = ImageDataset(train_dir, 
+            train_image_dir, transform, transform)
+    train_len, valid_len = int(len(dataset) * 0.8), int(len(dataset) * 0.2)
+    train, valid = Data.random_split(dataset, [train_len, valid_len])
+    #print(valid.shape)
     train_iter = Data.DataLoader(train, batchsize, shuffle=True)
-    test_iter = Data.DataLoader(test, batchsize, shuffle=True)
+    valid_iter = Data.DataLoader(valid, batchsize, shuffle=True)
     CELoss = nn.BCEWithLogitsLoss()
     for model in models:
         myfile = import_module('models.' + model)
@@ -46,25 +49,25 @@ if __name__ == '__main__':
             for X, y in train_iter:
                 yhat = Model(X).reshape(y.shape)
                 yhat = logsist(yhat)
-                loss = float(CELoss(yhat, y))
-                allloss += loss
+                loss = (CELoss(yhat, y))
+                allloss += float(loss)
                 sgd.zero_grad()
                 loss.backward()
                 sgd.step()
-                print('%f' % loss)
+                #print('%f' % loss)
             print('epoch %d loss is : %f' % (epoch + 1, allloss / len(train)))
-            ALLLOSS.append(allloss)
+            ALLLOSS.append(allloss / len(train))
             #if epoch % 2 == 0:
             with torch.no_grad():
-                test_yhat = []
-                test_y = []
-                for testx, testy in test_iter:
-                    test_yhat.append(Model(testx).reshape(testy.shape))
-                    test_y.append(testy)
-                test_yhat = logsist(torch.cat(test_yhat, dim=-1))
-                test_y = torch.cat(test_y, dim=-1)
-                acc = Accurate(test_yhat, test_y)
-                print('epoch %d: accurate %f (test)' % (epoch+1, acc))
+                valid_yhat = []
+                valid_y = []
+                for validx, validy in valid_iter:
+                    valid_yhat.append(Model(validx).reshape(validy.shape))
+                    valid_y.append(validy)
+                valid_yhat = logsist(torch.cat(valid_yhat, dim=-1))
+                valid_y = torch.cat(valid_y, dim=-1)
+                acc = Accurate(valid_yhat, valid_y)
+                print('epoch %d: accurate %f (valid)' % (epoch+1, acc))
             ALLACC.append(acc)
         plt.title(model)
         plt.plot([i for i in range(len(ALLACC))], ALLACC)
@@ -72,7 +75,7 @@ if __name__ == '__main__':
         plt.legend(['train_acc', 'train_loss'])
         plt.xlabel('epoch')
         plt.show()
-        TP, FP, FN, TN = getMatrix(test_yhat > 0.5, test_y)
+        TP, FP, FN, TN = getMatrix(valid_yhat > 0.5, valid_y)
         C = [[TP, FN], [FP, TN]]
         sns.set()
         f, ax = plt.subplots()
@@ -80,17 +83,32 @@ if __name__ == '__main__':
         ax.set_title(model + 'Mixed Matrix')
         ax.set_xlabel('predict')
         ax.set_ylabel('true')
-        roc = ROC(test_yhat, test_y)
+        plt.show()
+        roc = np.array(ROC(valid_yhat, valid_y))
         plt.plot(roc[:, 0], roc[:, 1])
         plt.title(model + 'roc')
         plt.xlabel('FPR')
         plt.ylabel('TPR')
         plt.show()
-        auc = AUC(test_yhat, test_y)
+        auc = np.array(AUC(valid_yhat, valid_y))
         plt.plot(auc[:, 0], auc[:, 1])
         plt.title(model + 'AUC')
         plt.xlabel('FPR')
         plt.ylabel('TPR')
         plt.show()
-
+        # 以下对真实测试集进行预测，可以提交到kaggle上
+        Test = TestImageDataset(test_dir, test_image_dir)
+        test_iter = Data.DataLoader(Test, batchsize, shuffle=False)
+        test_img_names = []
+        test_Y = []
+        for img_name, test_X in test_iter:
+            test_img_names.extend(img_name)
+            test_Y.append(Model(test_X).unsqueeze(-1))
+        test_Y = logsist(test_Y)
+        test_label = []
+        for i in test_Y:
+            label = 1 if i > 0.5 else -1
+            test_label.append(label)
+        result = pd.DataFrame(columns=['image_id', 'is_male'], data = [[i, j] for i, j in zip(test_img_names, test_label)])
+        result.to_csv('./' + model +'output/sample_submission.csv')
 
